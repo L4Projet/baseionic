1bc9b0ec2e8b4cb3dd0e97e11f7d99ea8985109a
hassandevonly <hassanebangourabusiness2707@gmail.com>
2024-04-01 15:06:19 +0000

    first commit

--- src/app/pages/accueil/accueil.page.ts
+++ src/app/pages/accueil/accueil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-accueil',
@@ -35,9 +36,13 @@ export class AccueilPage implements OnInit {
     },
     
   ]
-  constructor() { }
+  constructor(public router : Router) { }
 
   ngOnInit() {
+  }
+
+  moreDetails(){
+    this.router.navigate(['detail'])
   }
 
 }
